451477e8f43067404f37417cb877c041b0b0e470
Snail-Pop <92945612+Snail-Pop@users.noreply.github.com>
2022-10-03 14:47:09 -0700

    ADDED RAT AI
    Rat has two states:
    1: Run direct (Runs directly at the player)
    2: Run direct and leap (Leaps when close to player)
    
    V0.0.3
    main branch

--- Assets/Scripts/EnemyAIs/RatAI.cs
+++ Assets/Scripts/EnemyAIs/RatAI.cs
@@ -6,7 +6,9 @@ public class RatAI : MonoBehaviour
 {
     [SerializeField] 
     public GameObject target;
-    Vector2 maxVel = new Vector3(2,2);
+    float leapTimer = 0;
+    bool movWait = false;
+    float moveTimer = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,22 +16,61 @@ public class RatAI : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         Move(0);
     }
 
-
     void Move(int state){
-
+        
         switch(state){
-
             case 0:
-            Vector3 movDir = target.transform.position - transform.position; 
-            gameObject.GetComponent<Rigidbody2D>().AddForce(movDir * 0.01f);  
+            RunDirectAndLeap(7.5f, 3f, 0.3f, 10);
             break;
-
+            case 1:
+            RunDirect(10);
+            break;
         }
 
+    }
+
+    void RunDirect(float speed){
+        Vector3 movDir = target.transform.position - transform.position; 
+        float distance = movDir.magnitude;
+        transform.position += movDir.normalized / speed;
+    }
+
+    void RunDirectAndLeap(float minDistance, float leapCooldown, float movWaitTime, float speed){
+        
+        Vector3 movDir = target.transform.position - transform.position; 
+        float distance = movDir.magnitude;
+        
+        if(!movWait){
+            transform.position += movDir.normalized / speed;
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+        }
+        else if(moveTimer > movWaitTime){
+            movWait = false;
+            moveTimer = 0;
+        }
+        else{
+            moveTimer += Time.deltaTime;
+        }
+
+        if(distance < minDistance && leapCooldown < leapTimer){
+            Leap(10, movDir);
+            movWait = true;
+            leapTimer = 0;
+        }
+        else{
+            leapTimer += Time.deltaTime;
+            print("Leap waiting: " + leapTimer + ", movWait = " + movWait);
+        }
+    }
+
+    void Leap(float leapSpeed, Vector2 direction){
+
+        gameObject.GetComponent<Rigidbody2D>().AddForce(direction.normalized * leapSpeed, ForceMode2D.Impulse);
+        
     }
 }
